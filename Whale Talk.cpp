99d124ecd16de69b7db4a8177de5769a76298941
cdelangelm <cdelangelm@users.noreply.github.com>
2020-07-27 12:00:49 -0500

    Add project files.

---
+++ Whale Talk.cpp
@@ -0,0 +1,15 @@
+// Whale Talk.cpp project from the vector block lesson, Codecademy.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    // Whale, whale, whale
+    // What have we got here?
+}
+
